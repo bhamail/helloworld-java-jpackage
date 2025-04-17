6e8ae129f51ccb54812f5c25a1db7a51e8051305
Dan Rollo <danrollo@gmail.com>
2025-04-17 17:49:30 -0400

    Yay, copying a .deb to the host!

--- dan-fun.sh
+++ dan-fun.sh
@@ -7,7 +7,7 @@
 docker build --tag "deb-hello" -f installers/deb/deb-hello.dockerfile .
 
 # run the docker image to create the .deb installer
-docker run -v output:/output deb-hello
+docker run -v "$(pwd)/output":/output deb-hello /bin/bash /data/installers/deb/startup.sh
 
 # build the docker image used to create the .rpm installer
 #docker build --tag "rpm-hello" -f rpm-hello.dockerfile .
